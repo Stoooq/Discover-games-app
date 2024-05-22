03237140b2be3bfa6c63bcd5544199360ab50238
Milosz Glowacki <milosz.glow@gmail.com>
2024-05-22 21:05:57 +0200

    add sort selector

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,5 @@
+import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenres";
 
 export interface Platform {
 	id: number;
@@ -16,13 +16,12 @@ export interface Game {
 }
 
 const useGames = (
-	selectedGenre: Genre | null,
-	selectedPlatform: Platform | null
+	gameQuery: GameQuery
 ) =>
 	useData<Game>(
 		"/games",
-		{ params: { genres: selectedGenre?.id, parent_platforms: selectedPlatform?.id } },
-		[selectedGenre?.id, selectedPlatform?.id]
+		{ params: { genres: gameQuery.genre?.id, parent_platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder } },
+		[gameQuery]
 	);
 
 export default useGames;
