bf2682a5718acb7110061c6d016f8b3e494d5b68
Milosz Glowacki <milosz.glow@gmail.com>
2024-07-05 14:00:11 +0200

    fetch game details

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,13 +1,16 @@
-import { Outlet } from "react-router-dom"
-import NavBar from "../components/NavBar"
+import { Outlet } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import { Box } from "@chakra-ui/react";
 
 const Layout = () => {
-  return (
-    <div>
-      <NavBar />
-      <Outlet />
-    </div>
-  )
-}
+	return (
+		<div>
+			<NavBar />
+			<Box padding={5}>
+				<Outlet />
+			</Box>
+		</div>
+	);
+};
 
-export default Layout
+export default Layout;
